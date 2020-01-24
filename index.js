4092b10be8a4de1dc14910e6acbdec754c7c5d1d
Janes Roberto <kadoshmt2@gmail.com>
2020-01-23 21:08:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const db = require('./config/db')
+
+/** @type {con} */
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
+    .then('./api') 
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
+
+app.listen(3000, () => {
+    console.log('Backend executando...')
+})
